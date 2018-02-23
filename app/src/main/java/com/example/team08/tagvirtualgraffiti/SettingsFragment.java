8fbb8569afd74b24d37f178013f2cec46b1f0103
StonelaceMetro <ovak.3@osu.edu>
2018-02-23 04:25:32 -0500

    Added Debug Logging to Setting Activity/Fragment

--- app/src/main/java/com/example/team08/tagvirtualgraffiti/SettingsFragment.java
+++ app/src/main/java/com/example/team08/tagvirtualgraffiti/SettingsFragment.java
@@ -83,4 +83,75 @@ public class SettingsFragment extends Fragment implements View.OnClickListener {
                 break;
         }
     }
+
+
+
+
+
+
+
+
+
+    @Override
+    public void onAttach(Context context){
+        super.onAttach(context);
+        Log.d(TAG, "onAttach() called");
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate() called");
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState){
+        super.onActivityCreated(savedInstanceState);
+        Log.d(TAG, "onActivityCreated() called");
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume() called");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause() called");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop() called");
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        Log.d(TAG, "onDestroyView() called");
+    }
+
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() called");
+    }
+
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        Log.d(TAG, "onDetach() called");
+    }
 }
