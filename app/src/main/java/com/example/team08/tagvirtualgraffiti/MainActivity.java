a6f438e834f3d1c6490b7d821fc1489498a69185
StonelaceMetro <ovak.3@osu.edu>
2018-03-21 07:00:45 -0400

    Created basic UI fragments for profile and current place

--- app/src/main/java/com/example/team08/tagvirtualgraffiti/MainActivity.java
+++ app/src/main/java/com/example/team08/tagvirtualgraffiti/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.team08.tagvirtualgraffiti;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -22,6 +25,8 @@ public class MainActivity extends AppCompatActivity {
                     return true;
                 case R.id.nav_current_location:
                     mTextMessage.setText(R.string.title_current_location);
+                    swapFragment(new CurrentLocationFragment());
+
                     return true;
                 case R.id.nav_map:
                     mTextMessage.setText(R.string.title_map);
@@ -31,6 +36,8 @@ public class MainActivity extends AppCompatActivity {
                     return true;
                 case R.id.nav_profile:
                     mTextMessage.setText(R.string.title_profile);
+
+                    swapFragment(new ProfileFragment());
                     return true;
             }
             return false;
@@ -45,6 +52,18 @@ public class MainActivity extends AppCompatActivity {
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+
+
+
+    private void swapFragment(Fragment fragment){
+        FragmentManager fm = getSupportFragmentManager();
+
+        fm.beginTransaction()
+                .replace(R.id.main_fragment_container, fragment)
+                .commit();
+
     }
 
 }
---
+++ app/src/main/java/com/example/team08/tagvirtualgraffiti/ProfileFragment.java
@@ -0,0 +1,42 @@
+package com.example.team08.tagvirtualgraffiti;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+public class ProfileFragment extends Fragment {
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+
+}
