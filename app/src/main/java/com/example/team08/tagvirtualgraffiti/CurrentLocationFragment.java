a4be341c9998ef603b7c11cf45f90b50641eb35c
StonelaceMetro <ovak.3@osu.edu>
2018-03-23 11:12:38 -0400

    Able to acquire current location name

--- app/src/main/java/com/example/team08/tagvirtualgraffiti/CurrentLocationFragment.java
+++ app/src/main/java/com/example/team08/tagvirtualgraffiti/CurrentLocationFragment.java
@@ -2,15 +2,31 @@ package com.example.team08.tagvirtualgraffiti;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.PlaceLikelihood;
+import com.google.android.gms.location.places.PlaceLikelihoodBufferResponse;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 
 public class CurrentLocationFragment extends Fragment {
 
-    
+    private final String TAG = getClass().getSimpleName();
+    private TextView mPlaceNameView;
+    private ImageView mPlaceImageView;
+
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -22,7 +38,16 @@ public class CurrentLocationFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_current_location, container, false);
+        View v =inflater.inflate(R.layout.fragment_current_location, container, false);
+
+
+        mPlaceNameView = (TextView) v.findViewById(R.id.place_name);
+        mPlaceImageView = (ImageView) v.findViewById(R.id.place_image);
+
+
+        updateCurrentPlace();
+
+        return v;
     }
 
 
@@ -37,4 +62,49 @@ public class CurrentLocationFragment extends Fragment {
         super.onDetach();
     }
 
+
+    /**
+     * Returns the fragment's activity cast as MainActivity for access to MainActivity's members
+     *
+     * @return fragment's current activity cast as MainActivity
+     */
+    private MainActivity getMainActivity(){
+
+        return (MainActivity) getActivity();
+    }
+
+
+
+    private void updateCurrentPlace() {
+
+        if (getMainActivity().checkLocationPermission()) {
+
+            Task<PlaceLikelihoodBufferResponse> placeResult = getMainActivity().mPlaceDetectionClient.getCurrentPlace(null);
+
+            placeResult.addOnCompleteListener
+                    (new OnCompleteListener<PlaceLikelihoodBufferResponse>() {
+                        @Override
+                        public void onComplete(@NonNull Task<PlaceLikelihoodBufferResponse> task) {
+                            if (task.isSuccessful() && task.getResult() != null) {
+                                PlaceLikelihoodBufferResponse likelyPlaces = task.getResult();
+
+                                Place currentPlace = likelyPlaces.get(0).getPlace();
+
+
+                                if (mPlaceImageView != null) {
+                                    //TODO: use GeoDataClient to get image?
+                                }
+                                if (mPlaceNameView != null){
+                                    mPlaceNameView.setText(currentPlace.getName());
+                                }
+
+                            } else {
+                                Log.e(TAG, "Exception: %s", task.getException());
+                            }
+                        }
+                    });
+        } else {
+            Toast.makeText(getContext(), "Invalid Location Permissions", Toast.LENGTH_SHORT);
+        }
+    }
 }
