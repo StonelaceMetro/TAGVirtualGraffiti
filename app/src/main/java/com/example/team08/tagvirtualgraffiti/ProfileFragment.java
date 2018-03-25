26c66943a20f21023b05af459fc2ce8b19902469
Amanpreet Singh <amanpreetsingh3095@gmail.com>
2018-03-24 21:13:31 -0400

    Added tag image upload functionality

--- app/src/main/java/com/example/team08/tagvirtualgraffiti/ProfileFragment.java
+++ app/src/main/java/com/example/team08/tagvirtualgraffiti/ProfileFragment.java
@@ -1,9 +1,12 @@
 package com.example.team08.tagvirtualgraffiti;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -13,16 +16,32 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.IOException;
+import java.util.UUID;
+
+import static android.app.Activity.RESULT_OK;
 
 
 public class ProfileFragment extends Fragment implements View.OnClickListener{
 
     private Button mLogoutButton;
     private ImageButton mChangeTagButton;
+    private ImageView mTagImageView;
+
+    FirebaseStorage storage;
+    StorageReference storageReference;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -39,6 +58,7 @@ public class ProfileFragment extends Fragment implements View.OnClickListener{
         //NOTE: Button is derived from TextView whereas ImageButton is derived from ImageView
         mLogoutButton = (Button) v.findViewById(R.id.logout_button);
         mChangeTagButton = (ImageButton) v.findViewById(R.id.change_tag_button);
+        mTagImageView = (ImageView) v.findViewById(R.id.profile_tag_image);
 
         if(mLogoutButton != null){
             mLogoutButton.setOnClickListener(this);
@@ -94,8 +114,67 @@ public class ProfileFragment extends Fragment implements View.OnClickListener{
             case R.id.change_tag_button:
                 //TODO: open gallery, upload and scale photo
                 Toast.makeText(getActivity(), "Opening Gallery...", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent, "Select Picture"), 1);
+
                 break;
         }
     }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 1 && resultCode == RESULT_OK
+                && data != null && data.getData() != null )
+        {
+            Uri filePath = data.getData();
+            uploadToFirebase(filePath);
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), filePath);
+                mTagImageView.setImageBitmap(bitmap);
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void uploadToFirebase(Uri filePath) {
+        if(filePath != null)
+        {
+            storage = FirebaseStorage.getInstance();
+            storageReference = storage.getReference();
+            final ProgressDialog progressDialog = new ProgressDialog(getContext());
+            progressDialog.setTitle("Uploading...");
+            progressDialog.show();
+
+            StorageReference ref = storageReference.child("images/"+ TagApplication.mCurrentUser.getId());
+            ref.putFile(filePath)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            progressDialog.dismiss();
+                            Toast.makeText(getContext(), "Uploaded", Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            progressDialog.dismiss();
+                            Toast.makeText(getContext(), "Failed "+e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                            double progress = (100.0*taskSnapshot.getBytesTransferred()/taskSnapshot
+                                    .getTotalByteCount());
+                            progressDialog.setMessage("Uploaded "+(int)progress+"%");
+                        }
+                    });
+        }
+    }
 }
